b2a58f7d2930f451426000b8aa73b5c3b1ad240d
0xyshv <writetoyshv@gmail.com>
2023-08-27 15:10:50 +0530

    Updated primary button

--- src/aUSD/PrimaryButton.jsx
+++ src/aUSD/PrimaryButton.jsx
@@ -1,25 +1,44 @@
-const styles = {
-  primaryButton: {
-    color: "rgba(255, 255, 255, 0.8)",
-    fontSize: "16px",
-    padding: "5px 16px",
-    fontWeight: 400,
-    background: "none",
-    backgroundImage:
-      "linear-gradient(145deg, #016EDA, #6C1ECF, #016EDA, #6C1ECF)",
-    border: "1px solid rgba(255, 255, 255, 0.2)",
-    boxShadow: "0 2px 0 rgba(0, 0, 0, 0.02)",
-    height: "40px",
-    borderRadius: "40px",
-    lineHeight: "29px",
-    letterSpacing: "0.01em",
-    display: "flex",
-    alignItems: "center",
-    marginLeft: "300px",
-  },
-};
-return (
-  <button style={styles.primaryButton} type="button">
-    Primary Button
-  </button>
+const { title, loading, config, disabled } = props;
+
+const PrimaryButton = styled.button`
+  border: 0;
+
+  color: white;
+  background: ${loading || disabled ? "#36295C" : "#8247e5"};
+  border-radius: 5px;
+
+  height: 48px;
+  width: 100%;
+
+  font-size: 16px;
+  font-weight: bold;
+
+  transition: all 0.3s ease;
+
+  &:disabled {
+    cursor: not-allowed;
+  }
+`;
+
+const Loading = () => (
+  <img
+    width={40}
+    height={20}
+    src={`${config.ipfsPrefix}/bafkreib3s7t6npgjqrplyduxbcrnpx7rnncxzgmsgp5smo3byms272jkgm`}
+  />
 );
+
+return (
+  <PrimaryButton disabled={loading || disabled}>
+    {loading ? <Loading /> : title}
+  </PrimaryButton>
+);
+
+// props = {
+//   config: {
+//     ipfsPrefix: "https://ipfs.near.social/ipfs",
+//   },
+//   title: "Assets to supply",
+//   loading: false,
+//   disabled: true,
+// };
