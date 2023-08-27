c61f266018a1bef5bbc903ebf1110711ae650fe0
0xyshv <writetoyshv@gmail.com>
2023-08-27 14:43:20 +0530

    Add Card empty

---
+++ src/aUSD/Card/CardEmpty.jsx
@@ -0,0 +1,24 @@
+const { config } = props;
+
+const CardEmpty = styled.div`
+  min-height: 220px;
+
+  font-size: 14px;
+
+  display: grid;
+  place-content: center;
+`;
+
+return (
+  <>
+    <Widget
+      src={`${config.ownerId}/widget/AAVE.Card.Divider`}
+      props={{ config }}
+    />
+    <CardEmpty>{props.children}</CardEmpty>
+  </>
+);
+
+// props
+// "config":
+// {"children": "Nothing supplied yet"}
